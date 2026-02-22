a732b80e9cff1f02d9525ee1d6b353b5778d9cdb
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2026-02-22 13:29:32 +0100

    feat: calling api from FE

--- apps/task-api/src/main.rs
+++ apps/task-api/src/main.rs
@@ -1,13 +1,27 @@
-use axum::{Router, routing::get};
+use axum::{
+    Json, Router,
+    http::{HeaderValue, Method},
+    routing::get,
+};
+use serde::Serialize;
+use tower_http::cors::CorsLayer;
 
 #[tokio::main]
-
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new().route("/health", get(root)).layer(
+        CorsLayer::new()
+            .allow_origin("http://localhost:5000".parse::<HeaderValue>().unwrap())
+            .allow_methods([Method::GET]),
+    );
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello from rust!"
+#[derive(Serialize)]
+struct Health {
+    status: &'static str,
+}
+
+async fn root() -> Json<Health> {
+    Json(Health { status: "Ok" })
 }
--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,5 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { HealthService } from './core/services/health.service';
+import { catchError, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('web');
+
+  apiStatus = toSignal(
+    inject(HealthService)
+      .check()
+      .pipe(
+        map((res: any) => (res.status === 'Ok' ? 'up' : 'down')),
+        catchError(() => of('down')),
+      ),
+    { initialValue: 'checking' },
+  );
 }
