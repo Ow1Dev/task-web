a743b2e1ef55f272fb293469d9702b74b3ef709a
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2026-02-22 14:03:14 +0100

    feat: adding a print for the api

--- apps/task-api/src/main.rs
+++ apps/task-api/src/main.rs
@@ -14,6 +14,7 @@ async fn main() {
             .allow_methods([Method::GET]),
     );
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
