31249631ab59f1316344d9211c824dc50b20a9dc
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2026-03-18 19:21:56 +0100

    feat: fixing some small things

--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, inject, resource, signal } from '@angular/core';
 import { JsonPipe } from '@angular/common';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { RouterOutlet } from '@angular/router';
 import { TasksService } from './core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, JsonPipe],
+  imports: [JsonPipe],
   templateUrl: './app.html',
 })
 export class App {
